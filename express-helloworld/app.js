2d3f79c06acc4c49f7c8932e7ca96dda7652b630
Mohamed Atef <mohamedatef@hotmail.com>
2021-02-23 16:22:21 +0200

    add hello efg

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+
+app.get('/EFG', function (req, res) {
+  res.send('Hello EFG World!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
